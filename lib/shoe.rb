efea2b8b4cdc22616b3249c1694bab9438d512b7
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2017-05-16 14:49:00 -0400

    added ability to keep track of unique brands

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,14 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = Array.new
+
   def initialize(brand)
     @brand = brand
+
+    if !BRANDS.index(brand)
+      BRANDS << brand
+    end
   end
 
   def cobble
